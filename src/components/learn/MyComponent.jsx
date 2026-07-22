eda6732981c031245ca93f91e61972342e705b8a
Hoang Vu <minhhoangvu007@gmail.com>
2026-07-22 15:46:51 +0700

    Video 30: style.css prototype + MyComponent using css prototype

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,5 +1,12 @@
 const MyComponent = () => {
-  return <div class="name">Kevin & KOL update</div>;
+  return (
+    <>
+      <div class="name">Kevin & KOL update</div>
+      <div className="child" style={{ borderRadius: "10px" }}>
+        Kevin
+      </div>
+    </>
+  );
 };
 
 export default MyComponent;
