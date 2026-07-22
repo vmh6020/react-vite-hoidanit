4a976644e1c7b4d404bb9e906a91a202a91ad498
Hoang Vu <minhhoangvu007@gmail.com>
2026-07-22 16:06:09 +0700

    Video 31: Writing JavaScript inside the React function, return html

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,7 +1,17 @@
+import "./style.css";
 const MyComponent = () => {
+  const myName = "hoang";
+  const numb = 10;
+  const boo = true;
+  const numbers = [1, 2, 3, 4];
+  const student = {
+    name: "hoang",
+    age: 20,
+  };
   return (
     <>
       <div class="name">Kevin & KOL update</div>
+      <div>{JSON.stringify(numb)} KOL update</div>
       <div className="child" style={{ borderRadius: "10px" }}>
         Kevin
       </div>
